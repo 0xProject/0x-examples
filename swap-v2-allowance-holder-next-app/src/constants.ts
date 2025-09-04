f8ba552914a0e2a23e38780e8c8e40a067bf6cbd
Jessica Lin <jlin2700@gmail.com>
2025-09-04 16:45:57 -0700

    add swap v2 AH nex app

---
+++ swap-v2-allowance-holder-next-app/src/constants.ts
@@ -0,0 +1,140 @@
+import { Address } from "viem";
+
+export const PERMIT2_ADDRESS = "0x000000000022D473030F116dDEE9F6B43aC78BA3";
+
+export const MAGIC_CALLDATA_STRING = "f".repeat(130); // used when signing the eip712 message
+
+export const AFFILIATE_FEE = 100; // 1% affiliate fee. Denoted in Bps.
+export const FEE_RECIPIENT = "0x75A94931B81d81C7a62b76DC0FcFAC77FbE1e917"; // The ETH address that should receive affiliate fees
+
+export const MAINNET_EXCHANGE_PROXY =
+  "0xdef1c0ded9bec7f1a1670819833240f027b25eff";
+
+export const MAX_ALLOWANCE =
+  115792089237316195423570985008687907853269984665640564039457584007913129639935n;
+
+interface Token {
+  name: string;
+  address: Address;
+  symbol: string;
+  decimals: number;
+  chainId: number;
+  logoURI: string;
+}
+
+export const MAINNET_TOKENS: Token[] = [
+  {
+    chainId: 1,
+    name: "Wrapped Ether",
+    symbol: "WETH",
+    decimals: 18,
+    address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    logoURI:
+      "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/weth.svg",
+  },
+  {
+    chainId: 1,
+    name: "USD Coin",
+    symbol: "USDC",
+    decimals: 6,
+    address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    logoURI:
+      "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/usdc.svg",
+  },
+  {
+    chainId: 1,
+    name: "Dai - PoS",
+    symbol: "DAI",
+    decimals: 18,
+    address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    logoURI:
+      "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/dai.svg",
+  },
+  {
+    chainId: 1,
+    name: "FLOKI",
+    symbol: "FLOKI",
+    decimals: 9,
+    address: "0xcf0c122c6b73ff809c693db761e7baebe62b6a2e",
+    logoURI:
+      "https://raw.githubusercontent.com/trustwallet/assets/c37119334a24f9933f373c6cc028a5bdbad2ecb4/blockchains/ethereum/assets/0xcf0C122c6b73ff809C693DB761e7BaeBe62b6a2E/logo.png",
+  },
+];
+
+export const MAINNET_TOKENS_BY_SYMBOL: Record<string, Token> = {
+  weth: {
+    chainId: 1,
+    name: "Wrapped Ether",
+    symbol: "WETH",
+    decimals: 18,
+    address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    logoURI:
+      "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/weth.svg",
+  },
+  usdc: {
+    chainId: 1,
+    name: "USD Coin",
+    symbol: "USDC",
+    decimals: 6,
+    address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    logoURI:
+      "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/usdc.svg",
+  },
+  dai: {
+    chainId: 1,
+    name: "Dai - PoS",
+    symbol: "DAI",
+    decimals: 18,
+    address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    logoURI:
+      "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/dai.svg",
+  },
+  floki: {
+    chainId: 1,
+    name: "FLOKI",
+    symbol: "FLOKI",
+    decimals: 9,
+    address: "0xcf0c122c6b73ff809c693db761e7baebe62b6a2e",
+    logoURI:
+      "https://raw.githubusercontent.com/trustwallet/assets/c37119334a24f9933f373c6cc028a5bdbad2ecb4/blockchains/ethereum/assets/0xcf0C122c6b73ff809C693DB761e7BaeBe62b6a2E/logo.png",
+  },
+};
+
+export const MAINNET_TOKENS_BY_ADDRESS: Record<string, Token> = {
+  "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": {
+    chainId: 1,
+    name: "Wrapped Ether",
+    symbol: "WETH",
+    decimals: 18,
+    address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    logoURI:
+      "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/weth.svg",
+  },
+  "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": {
+    chainId: 1,
+    name: "USD Coin",
+    symbol: "USDC",
+    decimals: 6,
+    address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    logoURI:
+      "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/usdc.svg",
+  },
+  "0x6b175474e89094c44da98b954eedeac495271d0f": {
+    chainId: 1,
+    name: "Dai - PoS",
+    symbol: "DAI",
+    decimals: 18,
+    address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    logoURI:
+      "https://raw.githubusercontent.com/maticnetwork/polygon-token-assets/main/assets/tokenAssets/dai.svg",
+  },
+  "0xcf0c122c6b73ff809c693db761e7baebe62b6a2e": {
+    chainId: 1,
+    name: "FLOKI",
+    symbol: "FLOKI",
+    decimals: 9,
+    address: "0xcf0c122c6b73ff809c693db761e7baebe62b6a2e",
+    logoURI:
+      "https://raw.githubusercontent.com/trustwallet/assets/c37119334a24f9933f373c6cc028a5bdbad2ecb4/blockchains/ethereum/assets/0xcf0C122c6b73ff809C693DB761e7BaeBe62b6a2E/logo.png",
+  },
+};
---
+++ swap-v2-allowance-holder-next-app/src/utils/types.ts
@@ -0,0 +1,89 @@
+import { Address, type Hex } from "viem";
+import { EIP712TypedData } from "./signature";
+
+// This interface is subject to change as the API V2 endpoints aren't finalized.
+export interface PriceResponse {
+  sellToken: string;
+  buyToken: string;
+  sellAmount: string;
+  buyAmount: string;
+  grossSellAmount: string;
+  grossBuyAmount: string;
+  allowanceTarget: Address;
+  route: [];
+  fees: {
+    integratorFee: {
+      amount: string;
+      token: string;
+      type: "volume" | "gas";
+    } | null;
+    zeroExFee: {
+      billingType: "on-chain" | "off-chain";
+      feeAmount: string;
+      feeToken: Address;
+      feeType: "volume" | "gas";
+    };
+    gasFee: null;
+  } | null;
+  gas: string;
+  gasPrice: string;
+  auxiliaryChainData?: {
+    l1GasEstimate?: number;
+  };
+}
+
+// This interface is subject to change as the API V2 endpoints aren't finalized.
+export interface QuoteResponse {
+  sellToken: Address;
+  buyToken: Address;
+  sellAmount: string;
+  buyAmount: string;
+  grossSellAmount: string;
+  grossBuyAmount: string;
+  gasPrice: string;
+  allowanceTarget: Address;
+  route: [];
+  fees: {
+    integratorFee: {
+      amount: string;
+      token: string;
+      type: "volume" | "gas";
+    } | null;
+    zeroExFee: {
+      billingType: "on-chain" | "off-chain";
+      feeAmount: string;
+      feeToken: Address;
+      feeType: "volume" | "gas";
+    };
+    gasFee: null;
+  } | null;
+  auxiliaryChainData: {};
+  to: Address;
+  data: Hex;
+  value: string;
+  gas: string;
+  permit2: {
+    type: "Permit2";
+    hash: Hex;
+    eip712: EIP712TypedData;
+  };
+  transaction: V2QuoteTransaction;
+  tokenMetadata: {
+    buyToken: {
+      buyTaxBps: string | null;
+      sellTaxBps: string | null;
+    };
+    sellToken: {
+      buyTaxBps: string | null;
+      sellTaxBps: string | null;
+    };
+  };
+}
+
+export interface V2QuoteTransaction {
+  data: Hex;
+  gas: string | null;
+  gasPrice: string;
+  to: Address;
+  value: string;
+}
